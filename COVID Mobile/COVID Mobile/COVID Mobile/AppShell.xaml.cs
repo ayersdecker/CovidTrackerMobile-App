963a253245c01f7af38d6ae578f61cccacccb5e0
Decker Ayers <ayersdecker@gmail.com>
2022-10-09 01:50:36 -0400

    Page Update

--- COVID Mobile/COVID Mobile/COVID Mobile/AppShell.xaml.cs
+++ COVID Mobile/COVID Mobile/COVID Mobile/AppShell.xaml.cs
@@ -11,8 +11,7 @@ namespace COVID_Mobile
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            
         }
 
     }
